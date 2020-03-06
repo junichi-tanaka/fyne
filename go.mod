050cbd5350f52d5001b6281af412eae7514b9f7f
Junichi TANAKA <junsan45@gmail.com>
2020-03-07 02:23:01 +0900

    Add GCS upload feature

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.14
 require (
 	fyne.io/fyne v1.2.3
 	github.com/gotk3/gotk3 v0.4.0 // indirect
+	github.com/pkg/errors v0.8.1
 	github.com/sqweek/dialog v0.0.0-20200304031853-0dcd55bfe06a
+	gocloud.dev v0.19.0
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	_ "io/ioutil"
-	_ "os"
+	"io"
+	"os"
 	"path/filepath"
+	"time"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
@@ -12,24 +14,62 @@ import (
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
 
+	"github.com/pkg/errors"
 	osdialog "github.com/sqweek/dialog"
+	"gocloud.dev/blob"
+	_ "gocloud.dev/blob/gcsblob"
 )
 
 var (
-	listItems []string
+	gcsURL    string
+	uploadFiles []string
 )
 
+func uploadFile(ctx context.Context, urlstr, file string) error {
+	bucket, err := blob.OpenBucket(ctx, urlstr)
+	if err != nil {
+		return errors.Wrap(err, "failed to open blob")
+	}
+	defer bucket.Close()
+
+	key := filepath.Base(file)
+	w, err := bucket.NewWriter(ctx, key, nil)
+	if err != nil {
+		return errors.Wrap(err, "failed to create the writer")
+	}
+
+	f, err := os.Open(file)
+	if err != nil {
+		return errors.Wrap(err, "failed to open src file")
+	}
+	defer f.Close()
+
+	written, err := io.Copy(w, f)
+	if err != nil {
+		return errors.Wrap(err, "failed to copy file")
+	}
+	fmt.Println(written)
+
+	err = w.Close()
+	if err != nil {
+		return errors.Wrap(err, "failed to close the writer")
+	}
+
+	return nil
+}
+
 func main() {
+	ctx := context.Background()
+
 	app := app.New()
 
 	w := app.NewWindow("File Uploader")
 	w.Resize(fyne.NewSize(640, 480))
 
 	vbox := widget.NewVBox(
-			widget.NewLabel("This files will be uploaded!"),
-		)
+		widget.NewLabel("This files will be uploaded!"),
+	)
 	fileListContainer := widget.NewScrollContainer(vbox)
-
 
 	chooseButton := widget.NewButton("Choose Folder...", func() {
 		dir, err := osdialog.Directory().Title("Select Folder to upload the csv files").Browse()
@@ -44,24 +84,49 @@ func main() {
 			return
 		}
 		vbox.Children = make([]fyne.CanvasObject, 0)
+		uploadFiles = uploadFiles[:0]
 		for _, file := range files {
 			vbox.Append(
 				widget.NewLabel(file),
 			)
+			uploadFiles = append(uploadFiles, file)
 		}
 		vbox.Refresh()
 	})
 
+	gcsPathEntry := widget.NewEntry()
 	uploadButton := widget.NewButton("Upload!", func() {
-		if len(listItems) == 0 {
+		fmt.Println("upload!")
+		if len(uploadFiles) == 0 {
 			dialog.ShowError(fmt.Errorf("Please choose a folder!"), w)
 			return
 		}
+
+		ctxWithTimeout, _ := context.WithTimeout(ctx, time.Second*300)
+		gcsURL = gcsPathEntry.Text
+		for _, file := range uploadFiles {
+			fmt.Println(file)
+			err := uploadFile(ctxWithTimeout, gcsURL, file)
+			if err != nil {
+				dialog.ShowError(err, w)
+				return
+			}
+		}
 	})
 
+	gcsPathFormItem := widget.NewFormItem("gcs url", gcsPathEntry)
+	vbox2 := widget.NewVBox(
+		widget.NewForm(
+			gcsPathFormItem,
+		),
+		uploadButton,
+	)
+	gcsPathEntry.SetText("")
+	gcsPathEntry.SetPlaceHolder("gs://...")
+
 	box := fyne.NewContainerWithLayout(
-		layout.NewBorderLayout(chooseButton, uploadButton, nil, nil),
-		chooseButton, fileListContainer, uploadButton,
+		layout.NewBorderLayout(chooseButton, vbox2, nil, nil),
+		chooseButton, fileListContainer, vbox2,
 	)
 	w.SetContent(box)
 
